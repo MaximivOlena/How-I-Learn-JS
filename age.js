f6a5b9c19830bf96629bb758567b36a63984f305
MaximivOlena <67554164+MaximivOlena@users.noreply.github.com>
2020-09-17 14:32:48 +0300

    Add files via upload
    age calculation

---
+++ age.js
@@ -0,0 +1,7 @@
+// Запитує рік народження. Ask birthday year.
+let question = prompt( `Hey. Tell me your birthday year?` );
+let age = 2020 - question;
+
+// Підраховує вік. Calculation.
+alert ( `You are ${age} years old` );
+alert( `goodbye` );
